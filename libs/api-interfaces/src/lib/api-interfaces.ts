cd31d06f9f62b43a69217623104e720334147a01
Lyubimov Roman <r.lubimov@corp.mail.ru>
2020-10-27 17:07:53 +0300

    Adds docker db environment

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,18 @@
-export interface Message {
-  message: string;
+export interface Project {
+  ruTitle: string;
+  enTitle: string;
+  ruDescription?: string;
+  enDescription?: string;
+  pictures: ProjectPicture[];
+}
+
+export interface Picture {
+  url: string;
+}
+
+export interface ProjectPicture extends Picture {
+  ruTitle?: string;
+  enTitle?: string;
+  ruDescription?: string;
+  enDescription?: string;
 }
