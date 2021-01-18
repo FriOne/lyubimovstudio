e9f4959a2c0a139cd2805a80ce968997e3eec47a
Lyubimov Roman <lubimov.nsk@gmail.com>
2021-01-18 21:01:04 +0300

    Changes sendmail to nodemailer

--- apps/api/src/app/mail/mail.service.ts
+++ apps/api/src/app/mail/mail.service.ts
@@ -1,17 +1,34 @@
 import { Injectable, Logger } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
-import { promisify } from 'util';
-import sendmailFactory from 'sendmail';
-
-const sendMail = promisify(sendmailFactory({}));
+import { createTransport } from 'nodemailer';
 
 @Injectable()
 export class MailService {
   private readonly logger = new Logger(MailService.name);
 
-  constructor(private configService: ConfigService) {}
+  private user = this.configService.get('GMAIL_USER');
+  private password = this.configService.get('GMAIL_PASSWORD');
+  private transport;
+
+  constructor(private configService: ConfigService) {
+    if (!this.user) {
+      return;
+    }
+
+    this.transport = createTransport({
+      service: 'gmail',
+      auth: {
+        user: this.user,
+        pass: this.password,
+      }
+    });
+  }
 
   sendRequestEmail(name: string, phoneNumber: string, message?: string) {
+    if (!this.user) {
+      return;
+    }
+
     const ownerEmail = this.configService.get('OWNER_EMAIL');
 
     if (!ownerEmail) {
@@ -22,10 +39,10 @@ export class MailService {
 
     const messageHtml = message ? `<br/> Сообщение: <br/> ${message}` : '';
 
-    return sendMail({
-      from: 'server@lyubimovalexey.ru',
+    return this.transport.sendMail({
+      from: `LyubimovStudio <${this.user}>`,
       to: ownerEmail,
-      subject: `Новая заявка от "${name}"`,
+      subject: `Новая заявка ${name ? `от "${name}"`: ''}`,
       html: `
         Заявка для телефона <a href="tel:${phoneNumber}">${phoneNumber}</a>
         ${messageHtml}
